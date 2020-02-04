4482f1532c46200506ee031fd72e4dbb8c877c54
Martin Landa <landa.martin@gmail.com>
2020-02-04 19:40:32 +0100

    tuning vm/install.sh

--- _static/vm/install.sh
+++ _static/vm/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install git \
+sudo apt -y install git \
      autoconf2.13 \
      autotools-dev \
      bison \
@@ -44,22 +44,30 @@ sudo apt install git \
 
 sudo pip install pymodis sentinelsat pandas pywps
 
-# liblas
+# laszip
 wget https://github.com/LASzip/LASzip/releases/download/v2.2.0/laszip-src-2.2.0.tar.gz
 tar xvzf laszip-src-2.2.0.tar.gz
+(
 cd laszip-src-2.2.0/
 ./configure --prefix=/usr/local --includedir=/usr/local/include/laszip
 make
 sudo make install
+)
+rm -r laszip-src-2.2.0 laszip-src-2.2.0.tar.gz
 
+# liblas
 wget http://download.osgeo.org/liblas/libLAS-1.8.1.tar.bz2
 tar xvjf libLAS-1.8.1.tar.bz2
+(cd libLAS-1.8.1
 mkdir makefiles
 cd makefiles
 cmake -G "Unix Makefiles" ../ -DWITH_LASZIP=YES
 make
 sudo make install
+)
+rm -r libLAS-1.8.1 libLAS-1.8.1.tar.bz2
 
+# pywps flask
 cd /home/user
 git clone https://github.com/geopython/pywps-flask.git
 chown user:user pywps-flask -R
