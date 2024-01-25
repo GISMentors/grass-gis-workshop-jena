d9925c9905475b1b264999f63149e5d9dd7319ab
Martin Landa <landa.martin@gmail.com>
2024-01-25 22:22:14 +0100

    bump version to 4.0

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ copyright = u'2018-%d Martin Landa and GISMentors.eu' % get_year()
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '4.0'
 # The full version, including alpha/beta/rc tags.
 release = '%s' % version
 
