efdbd6d00d3972a434af7fd31e56477e7ed32e17
GitHub Action <action@github.com>
2024-02-03 17:50:18 +0000

    Add changes for e1d1ba3c6de9679c55ba1e96dc62ca4899a0c77b

--- _static/scripts/modis_v1.py
+++ _static/scripts/modis_v1.py
@@ -1,82 +1,87 @@
+#!/usr/bin/env python3
+
+import sys
 import os
+import atexit
+import tempfile
+from subprocess import PIPE
+from datetime import datetime
 
-from pywps import Process, LiteralInput, LiteralOutput
+from grass.pygrass.modules import Module
+from grass.script import parse_key_val
+from pywps import Process, LiteralInput, LiteralOutput, Format
 
-__author__ = 'Martin Landa'
 
 class ModisV1(Process):
+
     def __init__(self):
-        inputs = [LiteralInput('start', 'Start date (eg. 2019-03-01)',
-                               data_type='string'),
-                  LiteralInput('end', 'End date (eg. 2019-04-01)',
-                               data_type='string')
-        ]
-        outputs = [LiteralOutput('stats', 'Computed LST statistics',
-                                 data_type='string')
-        ]
+        inputs = list()
+        outputs = list()
+
+        inputs.append(LiteralInput('start', 'Start date (eg. 2019-03-01)',
+                                   data_type='string'))
+        inputs.append(LiteralInput('end', 'End date (eg. 2019-03-01)',
+                                   data_type='string'))
+
+        outputs.append(LiteralOutput('output', 'Computed LST statistics',
+                                     data_type='string'))
 
         super(ModisV1, self).__init__(
             self._handler,
-            identifier='modis-v1',
-            version='0.1',
-            title="Modis process (v1)",
-            abstract='The process uses the GRASS GIS to compute LST ' \
-            'statistics for given period in 2019 for Germany',
-            profile='',
+            identifier="modis-v1",
+            title="modis-date-stats",
             inputs=inputs,
             outputs=outputs,
+            # here you could also specify the GRASS location, for example:
+            # grass_location="EPSG:5514",
+            abstract="Computes LST stats for given period (limited to Germany and 2023).",
+            version="1.0",
             store_supported=True,
-            status_supported=True,
-            grass_location="/home/user/grassdata/germany-modis"
-        )
+            status_supported=True)
 
-    def check_date(self, date_str):
-        from datetime import datetime
+    @staticmethod
+    def _handler(request, response):
+        def check_date(date_str):
+            d = datetime.strptime(date_str, '%Y-%m-%d')
+            if d.year != 2023:
+                raise Exception("Only year 2023 allowed")
 
-        d = datetime.strptime(date_str, '%Y-%m-%d')
-        if d.year != 2019:
-            raise Exception("Only year 2019 allowed")
+        check_date(request.inputs['start'][0].data)
+        check_date(request.inputs['end'][0].data)
 
-    def _handler(self, request, response):
-        from subprocess import PIPE
+        Module("t.rast.series",
+               overwrite=True,
+               input="modis_c@PERMANENT",
+               method="average",
+               order="start_time",
+               nprocs=1,
+               memory=300,
+               where="start_time > '{}' and start_time < '{}'".format(
+                   request.inputs["start"][0].data,
+                   request.inputs["end"][0].data),
+               output="t_rast_series_out",
+               file_limit=1000)
 
-        import grass.script as gs
-        from grass.pygrass.modules import Module
-        from grass.exceptions import CalledModuleError
-        
-        start = request.inputs['start'][0].data
-        end = request.inputs['end'][0].data
-        self.check_date(start)
-        self.check_date(end)
+        m = Module("r.univar",
+                   flags="g",
+                   overwrite=True,
+                   map="t_rast_series_out",
+                   percentile=90,
+                   nprocs=1,
+                   separator="pipe",
+                   stdout_=PIPE)
 
-        output = 'modis_response'
-        
-        # be silent
-        os.environ['GRASS_VERBOSE'] = '0'
-
-        # need to set computation region (would be nice g.region strds or t.region)
-        Module('g.region', raster='c_001')
-        try:
-            Module('t.rast.series',
-                   input='modis_c@PERMANENT',
-                   output=output,
-                   method='average',
-                   where="start_time > '{start}' and start_time < '{end}'".format(
-                       start=start, end=end
-            ))
-        except CalledModuleError:
-            raise Exception('Unable to compute statistics')
-
-        ret = Module('r.univar',
-                     flags='g',
-                     map=output,
-                     stdout_=PIPE
-        )
-        stats = gs.parse_key_val(ret.outputs.stdout)
-        
+        stats = parse_key_val(m.outputs.stdout, val_type=float)
         outstr = 'Min: {0:.1f};Max: {1:.1f};Mean: {2:.1f}'.format(
-            float(stats['min']), float(stats['max']), float(stats['mean'])
+            stats['min'], stats['max'], stats['mean']
         )
-        response.outputs['stats'].data = outstr
+        response.outputs['output'].data = outstr        
 
         return response
+
+
+if __name__ == "__main__":
+    from pywps.app.Service import Service
+
+    processes = [Model()]
+    application = Service(processes)
